0d665938a6e8f8c306e66f449af4866165812c10
Fabio Amaral <fabio.amaral@yahoo.com.br>
2025-05-31 16:56:11 -0300

    Criado projeto 1 - front end 2

---
+++ 2025_05_19/Front-end-2/src/projeto-1/app.js
@@ -0,0 +1,8 @@
+console.log(document.querySelector('h1').textContent);
+document.querySelector('h1').textContent = 'ESALQ';
+document.title = 'MBA';
+
+let tagH3 = document.createElement('h3');
+tagH3.textContent = 'Engenharia de Software';
+document.body.appendChild(tagH3);
+
